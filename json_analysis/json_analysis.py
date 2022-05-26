106e1ff1869dd8c5522d7aa0dfb823d0731c8663
unknown <3454441686@qq.com>
2022-05-26 11:37:51 +0800

    Update json_analysis.py

--- json_analysis/json_analysis.py
+++ json_analysis/json_analysis.py
@@ -3,41 +3,57 @@ import os
 from pickle import NONE
 import pandas as pd
 
-def read_json(file_path):
+def Read_json(file_path):
     with open(file_path) as f:
         return json.load(f)
     
-def extract_info():
-    info=[]
-    name=['total_reads','total_bases','q20_bases','q30_bases','q20_rate','q30_rate','gc_content','duplication']
+def Extract_info(col_name,col_info):
+    value=[]
+    key=['before_total_reads','before_total_bases','before_q20_bases','before_q30_bases','before_q20_rate','before_q30_rate','before_gc_content','before_duplication','after_total_reads','after_total_bases','after_q20_bases','after_q30_bases','after_q20_rate','after_q30_rate','after_gc_content','after_duplication']
     everyKey="summary"
-    info.append(data.get(everyKey).get('after_filtering').get('total_reads'))
-    info.append(data.get(everyKey).get('after_filtering').get('total_bases'))
-    info.append(data.get(everyKey).get('after_filtering').get('q20_bases'))
-    info.append(data.get(everyKey).get('after_filtering').get('q30_bases'))
-    info.append(data.get(everyKey).get('after_filtering').get('q20_rate'))
-    info.append(data.get(everyKey).get('after_filtering').get('q30_rate'))
-    info.append(data.get(everyKey).get('after_filtering').get('gc_content'))
-    info.append(data.get('duplication').get('rate'))
-    df_append = pd.DataFrame(list(zip(name,info)), columns = ['name','info'])
-    return df_append
+    value.append(data.get(everyKey).get('before_filtering').get('total_reads'))
+    value.append(data.get(everyKey).get('before_filtering').get('total_bases'))
+    value.append(data.get(everyKey).get('before_filtering').get('q20_bases'))
+    value.append(data.get(everyKey).get('before_filtering').get('q30_bases'))
+    value.append(data.get(everyKey).get('before_filtering').get('q20_rate'))
+    value.append(data.get(everyKey).get('before_filtering').get('q30_rate'))
+    value.append(data.get(everyKey).get('before_filtering').get('gc_content'))
+    value.append(data.get('duplication').get('rate'))
+    value.append(data.get(everyKey).get('after_filtering').get('total_reads'))
+    value.append(data.get(everyKey).get('after_filtering').get('total_bases'))
+    value.append(data.get(everyKey).get('after_filtering').get('q20_bases'))
+    value.append(data.get(everyKey).get('after_filtering').get('q30_bases'))
+    value.append(data.get(everyKey).get('after_filtering').get('q20_rate'))
+    value.append(data.get(everyKey).get('after_filtering').get('q30_rate'))
+    value.append(data.get(everyKey).get('after_filtering').get('gc_content'))
+    value.append(data.get('duplication').get('rate'))
+    extracted_info = pd.DataFrame(list(zip(key,value)), columns = [col_name,col_info])
+    return extracted_info
 
-def df_Append():
-    tmp=pd.concat([df, df_append], axis=1)
+def Df_Append():
+    tmp=pd.concat([df, extracted_info], axis=1)
     return tmp
 
-def writer(csv_name):
+def File_path(*args):
+    list=[]
+    for file in args:
+        list.append(file)
+    return list
+def Writer(things,csv_name):
     current_dir = os.path.abspath('C:\\Users\\Jeff\\OneDrive\\桌面\\')
     file_name = os.path.join(current_dir,csv_name)
-    df.to_csv(file_name,index=None,index_label=None)
+    things.to_csv(file_name,index=None,index_label=None)
 
 if __name__ == '__main__':
-    json_path=['C:\\Users\\Jeff\\OneDrive\\桌面\\B1P1_L2_801D73_fastp.json',
-        'C:\\Users\\Jeff\\OneDrive\\桌面\\B1P1_L2_801D73_fastp.json']
-    csv_path="fastp_json_extract_info.csv"
+    json_path=File_path('C:\\Users\\Jeff\\OneDrive\\桌面\\L5-P_fastp.json',
+        'C:\\Users\\Jeff\\OneDrive\\桌面\\L3-P_fastp.json',
+        'C:\\Users\\Jeff\\OneDrive\\桌面\\L2-P_fastp.json',
+        'C:\\Users\\Jeff\\OneDrive\\桌面\\B6-P_fastp.json',
+        'C:\\Users\\Jeff\\OneDrive\\桌面\\B5-P_fastp.json',
+        'C:\\Users\\Jeff\\OneDrive\\桌面\\B4-P_fastp.json')
     df = pd.DataFrame()
     for i in range(len(json_path)):
-        data=read_json(json_path[i])
-        df_append=extract_info()
-        df=df_Append()
-        writer(csv_path)
+        data=Read_json(json_path[i])
+        extracted_info=Extract_info(json_path[i].split("\\")[-1]+"_name",json_path[i].split("\\")[-1]+"_info")
+        df=Df_Append()
+        Writer(df,"fastp_json.csv")
